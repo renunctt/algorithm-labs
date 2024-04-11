2bd83ef6486b423f8fafeeaffd3f9b3920e9f1ec
renunctt <sultanabibollaev@gmail.com>
2024-04-11 15:25:49 +0500

    update 9 lab

--- lab-9/main.cpp
+++ lab-9/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int *input_nums(int length) {
+int length = 0;
+
+int *input_nums() {
   int i = 0;
   int *nums = new int[length];
   while (i < length)
@@ -17,7 +19,7 @@ void print_index(int index, int count) {
   cout << "\nloop count: " << count << '\n';
 }
 
-void linear_search(int *nums, int length) {
+void linear_search(int *nums) {
   int search_num, i, count;
   int search_index = -1;
   i = count = 0;
@@ -36,7 +38,7 @@ void linear_search(int *nums, int length) {
   print_index(search_index, count);
 }
 
-void binary_search(int *nums, int length) {
+void binary_search(int *nums) {
   int search_num, i, count;
   int search_index = -1;
   int mid = length / 2;
@@ -56,14 +58,66 @@ void binary_search(int *nums, int length) {
     }
   }
 
+  cout << search_index << '\n';
+
   print_index(search_index, count);
 }
 
-int main() {
-  int length;
-  cin >> length;
-  int *nums = input_nums(length);
+void max_elem(int *nums) {
+  int max, index, i = 1;
+  index = 0;
+  max = nums[0];
+  while (i < length) {
+    if (max < nums[i++]) {
+      max = nums[i];
+      index = i;
+    }
+  }
+  cout << "max number: " << max << " index: " << index << '\n';
+}
 
-  linear_search(nums, length);
-  binary_search(nums, length);
+void min_elem(int *nums) {
+  int min, index, i = 1;
+  index = 0;
+  min = nums[0];
+  while (i < length) {
+    if (min > nums[i++]) {
+      min = nums[i];
+      index = i;
+    }
+  }
+  cout << "min number: " << min << " index: " << index << '\n';
+}
+
+void isNumsSimetria(int *nums) {
+  int start, end = length - 1;
+  start = 0;
+  while (start < end)
+    if (nums[start++] != nums[end--]) {
+      cout << "no\n";
+      return;
+    }
+  cout << "yes\n";
+}
+
+void print_nums_index(int *nums) {
+  int i = 0;
+  while (i < length)
+    cout << "index: " << i << " number: " << nums[i++] << '\n';
+}
+
+int main() {
+  cin >> length;
+  int *nums = input_nums();
+
+  linear_search(nums);
+
+  // Работает только с сортированном массивом
+  binary_search(nums);
+
+  max_elem(nums);
+  min_elem(nums);
+  isNumsSimetria(nums);
+  print_nums_index(nums);
+  cout << "min index: 0 number: " << nums[0] << '\n';
 }
