db3501d117c14d5d86fbbc070dff44501311ad2f
renunctt <sultanabibollaev@gmail.com>
2024-04-18 14:07:01 +0500

    2 Task Sort matrix by row summ

--- lab-10/main.cpp
+++ lab-10/main.cpp
@@ -2,6 +2,16 @@
 #include <iostream>
 using namespace std;
 
+void sort(int *array, int length) {
+  for (int i = 0; i < length - 1; ++i)
+    for (int j = i + 1; j < length; ++j)
+      if (array[i] > array[j]) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+}
+
 int main() {
   int c, r;
   cin >> c >> r;
@@ -11,5 +21,15 @@ int main() {
     for (int j = 0; j < r; ++j)
       cin >> M.at(i, j);
 
+  cout << "1. Summ matrix\n";
   cout << M.summ() << '\n';
+
+  cout << "2. Sort matrix by row summ\n";
+  int *rows_summ = new int[c];
+  for (int i = 0; i < c; ++i)
+    rows_summ[i] = M.summ_row(i);
+  sort(rows_summ, c);
+  for (int i = 0; i < c; ++i)
+    cout << rows_summ[i] << ' ';
+  cout << '\n';
 }
