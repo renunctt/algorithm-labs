0f11ecc99ced1b087af526935268582f4fce442c
renunctt <sultanabibollaev@gmail.com>
2024-04-18 14:27:11 +0500

    add utilities

---
+++ lab-10/utilities.cpp
@@ -0,0 +1,18 @@
+#include "utilities.h"
+#include <iostream>
+
+void sort(int *array, int length) {
+  for (int i = 0; i < length - 1; ++i)
+    for (int j = i + 1; j < length; ++j)
+      if (array[i] > array[j]) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+}
+
+void print_array(int *array, int length) {
+  for (int i = 0; i < length; ++i)
+    std::cout << array[i] << ' ';
+  std::cout << '\n';
+}
---
+++ lab-10/utilities.h
@@ -0,0 +1,7 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+void sort(int *array, int length);
+void print_array(int *array, int length);
+
+#endif
